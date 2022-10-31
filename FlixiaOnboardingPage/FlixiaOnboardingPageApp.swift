dc526fd977d157506efcd80ddd24fc1d8f124e50
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-10-31 16:54:10 +0100

    Initial Commit

---
+++ FlixiaOnboardingPage/FlixiaOnboardingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixiaOnboardingPageApp.swift
+//  FlixiaOnboardingPage
+//
+//  Created by Amaechi Chukwu on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlixiaOnboardingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
